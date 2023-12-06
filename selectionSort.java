1471ff12f6397e158e87f4ee9cbf0d7013a979fd
ritik chaurasia <ritikchaurasia0707@gmailcom>
2023-12-06 21:13:20 +0530

    first commit

---
+++ selectionSort.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+  public static void printArray(int arr[]) {
+    System.out.print("The sorted element is:");
+
+    for (int j = 0; j < arr.length; j++) {
+
+      System.out.print(arr[j] + " ");
+    }
+
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+
+    System.out.print("Enter the size of the array:");
+    Scanner sc = new Scanner(System.in);
+    int size = sc.nextInt();
+    int arr[] = new int[size];
+    System.out.println("Enter the array elements:");
+    // Taking input
+    for (int i = 0; i < size; i++) {
+      arr[i] = sc.nextInt();
+    }
+    // logic
+    for (int j = 0; j < size - 1; j++) {
+      int smallest = j;
+      for (int k = j + 1; k < size; k++) {
+        if (arr[smallest] > arr[k]) {
+          smallest = k;
+
+        }
+      }
+      // Swap
+      int temp = arr[smallest];
+      arr[smallest] = arr[j];
+      arr[j] = temp;
+    }
+    printArray(arr);
+  }
+}
